016f3abe20830e95e4f4de4e11a64c639416fffb
Ruslan <38631709+byrvr@users.noreply.github.com>
2025-09-11 14:53:18 +0400

    chore(logo): remove unused import

--- platform/ui-next/src/components/Icons/Sources/OHIFLogoColorDarkBackground.tsx
+++ platform/ui-next/src/components/Icons/Sources/OHIFLogoColorDarkBackground.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import type { IconProps } from '../types';
 
 export const OHIFLogoColorDarkBackground = () => (
   <img src="https://obz.ai/logo/obzai-logo-dark.png" width="200px" alt="OBZ.ai Logo" />
