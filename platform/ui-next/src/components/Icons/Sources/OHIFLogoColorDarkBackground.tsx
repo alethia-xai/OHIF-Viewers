1760bd239e4e0a748e7f371ed43e594b8a847280
Ruslan <38631709+byrvr@users.noreply.github.com>
2025-09-11 14:52:57 +0400

    fix(logo): add logo to local

--- platform/ui-next/src/components/Icons/Sources/OHIFLogoColorDarkBackground.tsx
+++ platform/ui-next/src/components/Icons/Sources/OHIFLogoColorDarkBackground.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { IconProps } from '../types';
 
-export const OHIFLogoColorDarkBackground = (props: IconProps) => (
+export const OHIFLogoColorDarkBackground = () => (
   <img src="https://obz.ai/logo/obzai-logo-dark.png" width="200px" alt="OBZ.ai Logo" />
 
 );
